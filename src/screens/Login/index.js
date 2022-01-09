619abaaee20a977db191ab702481041c322fa4f5
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2022-01-09 17:18:25 -0300

    login rev.2

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import { Container, Header, HeaderTitle } from './styled'
+import React, { useState } from 'react'
+import { 
+  Container, 
+  Header, 
+  HeaderTitle,
+  Menu,
+  MenuItem,
+  MenuItemText
+} from './styled'
 
 const Page = () => {
-    return (
-     <Container>
-     <Header>
-     <HeaderTitle>
-     DevsUber 
-     </HeaderTitle>
-     </Header>
-     </Container>
-    )
-  }
 
-  export default Page
+  const [activeMenu, setActiveMenu] = useState('signin')
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>Uber Clone</HeaderTitle>
+      </Header>
+
+      <Menu>
+        <MenuItem active={activeMenu =='signin'} onPress={()=>setActiveMenu('signin')} underlayColor="transparent"> 
+        {/*active={true seleciona o item} */}
+          <MenuItemText>Login</MenuItemText>
+        </MenuItem>
+
+        <MenuItem active={activeMenu == 'signup'} onPress={()=>setActiveMenu('signup')} underlayColor="transparent">
+          <MenuItemText>Cadastrar</MenuItemText>
+        </MenuItem>
+      </Menu>
+
+
+      
+    </Container>
+  );
+};
+
+export default Page;
